864ec750878b945720116f9370364b7c06945434
ThuongHoai <thefightinggirl178@gmail.com>
2023-09-08 09:43:48 +0700

    update

--- CodingChallenge4/insex.js
+++ CodingChallenge4/insex.js
@@ -1,8 +1,9 @@
 var bill = 275
 var tip
 if (bill >= 50 && bill < 300)
-    tip = (bill * 0.15).toFixed(2)
+    tip = (bill * 0.15)
 else
-    tip = bill * 0.2.toFixed(2)
+    tip = bill * 0.2
 
-console.log(`The bill was ${bill}, the tip was ${tip} and the total value `);
+var total=bill+tip
+console.log(`The bill was ${bill}, the tip was ${tip} and the total value ${total}`);
